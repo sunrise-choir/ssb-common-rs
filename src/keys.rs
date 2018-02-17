d7c536d55cd1409fc88cfecc2f3153f3f80fc488
AljoschaMeyer <mail@aljoscha-meyer.de>
2018-02-17 23:37:08 +0100

    Implement key types

--- src/keys.rs
+++ src/keys.rs
@@ -1,4 +1,5 @@
 //! TODO update description once you figured out how to represent stuff in types
+//! - as of now, only one primitive (ed25519)
 //!
 //! Operations for dealing with the public and secret keys used by ssb. This
 //! mostly deals with their representation in base64 and with a suffix
@@ -10,17 +11,16 @@
 //! encodings that include a suffix indicating the cryptographic primitive.
 
 use std::convert::From;
+use std::ops::{Index, Range, RangeTo, RangeFrom, RangeFull};
 
 use sodiumoxide::crypto::sign;
-
-// TODO serde (de)serialization traits?
 
 /// An ssb public key. This type abstracts over the fact that ssb can support
 /// multiple cryptographic primitives.
 ///
 /// New instances can be either created via a `From` implementation, or through
 /// one of the parsing functions. // TODO are there multiple? What about key generation functions?
-// TODO derive traits?
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
 pub enum PublicKey {
     /// An [Ed25519](http://ed25519.cr.yp.to/) public key, as used by
     /// `sodiumoxide::crypto::sign`.
@@ -28,18 +28,129 @@ pub enum PublicKey {
 }
 
 impl PublicKey {
-    // TODO keep this?
-    // /// Returns which cryptographic primitive is used for this `PublicKey`.
-    // fn crypto_primitive(&self) -> CryptoPrimitive {
-    //     match *self {
-    //         PublicKey::Ed25519(_) => CryptoPrimitive::Ed25519,
-    //     }
-    // }
+    /// Return the length (in bytes) of the key.
+    pub fn len(&self) -> usize {
+        match *self {
+            PublicKey::Ed25519(_) => sign::PUBLICKEYBYTES,
+        }
+    }
+
+    /// Verify the given signed message with this `PublicKey`. On success,
+    /// return a `Vec<u8>` containing the message without the signature.
+    pub fn verify(&self, signed_message: &[u8]) -> Result<Vec<u8>, ()> {
+        match *self {
+            PublicKey::Ed25519(ref pk) => sign::verify(signed_message, pk),
+        }
+    }
+
+    /// Verify the given message with this `PublicKey` and the given `Signature`.
+    ///
+    /// # Panics
+    /// Panics if this `PublicKey` and the `Signature` use different
+    /// cryptographic primitives.
+    pub fn verify_detached(&self, signature: &Signature, signed_message: &[u8]) -> bool {
+        match *self {
+            PublicKey::Ed25519(ref pk) => {
+                match *signature {
+                    Signature::Ed25519(ref sig) => sign::verify_detached(sig, signed_message, pk),
+                }
+            }
+        }
+    }
+
+    /// Verify the given message with this `PublicKey` and the given `Signature`.
+    /// Return `None` if this `PublicKey` and the `Signature` use different
+    /// cryptographic primitives.
+    pub fn try_verify_detached(&self,
+                               signature: &Signature,
+                               signed_message: &[u8])
+                               -> Option<bool> {
+        match *self {
+            PublicKey::Ed25519(ref pk) => {
+                match *signature {
+                    Signature::Ed25519(ref sig) => {
+                        Some(sign::verify_detached(sig, signed_message, pk))
+                    }
+                }
+            }
+        }
+    }
+
+    /// Return whether this `PublicKey` uses the ed25519 cryptographic primitive.
+    pub fn is_ed25519(&self) -> bool {
+        match *self {
+            PublicKey::Ed25519(_) => true,
+        }
+    }
+
+    /// Return whether this `PublicKey` uses the same cryptographic primitive as
+    /// the given `SecretKey`.
+    pub fn matches_secret_key(&self, secret_key: &SecretKey) -> bool {
+        match *self {
+            PublicKey::Ed25519(_) => secret_key.is_ed25519(),
+        }
+    }
+
+    /// Return whether this `PublicKey` uses the same cryptographic primitive as
+    /// the given `Signature`.
+    pub fn matches_signature(&self, signature: &Signature) -> bool {
+        match *self {
+            PublicKey::Ed25519(_) => signature.is_ed25519(),
+        }
+    }
 }
 
 impl From<sign::PublicKey> for PublicKey {
     fn from(ed25519_pk: sign::PublicKey) -> PublicKey {
         PublicKey::Ed25519(ed25519_pk)
+    }
+}
+
+impl AsRef<[u8]> for PublicKey {
+    fn as_ref(&self) -> &[u8] {
+        match *self {
+            PublicKey::Ed25519(ref pk) => pk.as_ref(),
+        }
+    }
+}
+
+/// Allows to access the byte contents of a `PublicKey` as a slice.
+impl Index<Range<usize>> for PublicKey {
+    type Output = [u8];
+    fn index(&self, _index: Range<usize>) -> &[u8] {
+        match *self {
+            PublicKey::Ed25519(ref pk) => pk.index(_index),
+        }
+    }
+}
+
+/// Allows to access the byte contents of a `PublicKey` as a slice.
+impl Index<RangeTo<usize>> for PublicKey {
+    type Output = [u8];
+    fn index(&self, _index: RangeTo<usize>) -> &[u8] {
+        match *self {
+            PublicKey::Ed25519(ref pk) => pk.index(_index),
+        }
+    }
+}
+
+/// Allows to access the byte contents of a `PublicKey` as a slice.
+impl Index<RangeFrom<usize>> for PublicKey {
+    type Output = [u8];
+    fn index(&self, _index: RangeFrom<usize>) -> &[u8] {
+        match *self {
+            PublicKey::Ed25519(ref pk) => pk.index(_index),
+        }
+    }
+}
+
+/// Allows to access the byte contents of a `PublicKey` as a slice.
+impl Index<RangeFull> for PublicKey {
+    type Output = [u8];
+    fn index(&self, _index: RangeFull) -> &[u8] {
+        match *self {
+            PublicKey::Ed25519(ref pk) => pk.index(_index),
+        }
     }
 }
 
@@ -50,7 +161,7 @@ impl From<sign::PublicKey> for PublicKey {
 ///
 /// New instances can be either created via a `From` implementation, or through
 /// one of the parsing functions. // TODO are there multiple? What about key generation functions?
-// TODO derive traits?
+#[derive(Clone, PartialEq, Eq, Debug)]
 pub enum SecretKey {
     /// An [Ed25519](http://ed25519.cr.yp.to/) secret key, as used by
     /// `sodiumoxide::crypto::sign`.
@@ -58,18 +169,158 @@ pub enum SecretKey {
 }
 
 impl SecretKey {
-    // TODO keep this?
-    // /// Returns which cryptographic primitive is used for this `SecretKey`.
-    // fn crypto_primitive(&self) -> CryptoPrimitive {
-    //     match *self {
-    //         SecretKey::Ed25519(_) => CryptoPrimitive::Ed25519,
-    //     }
-    // }
+    /// Return the length (in bytes) of the key.
+    pub fn len(&self) -> usize {
+        match *self {
+            SecretKey::Ed25519(_) => sign::SECRETKEYBYTES,
+        }
+    }
+
+    /// Sign a message with this `SecretKey`, returning the resulting message in
+    /// a `Vec<u8>`.
+    pub fn sign(&self, message: &[u8]) -> Vec<u8> {
+        match *self {
+            SecretKey::Ed25519(ref sk) => sign::sign(message, sk),
+        }
+    }
+
+    /// Sign a message with this `SecretKey`, returning only the signature.
+    pub fn sign_detached(&self, message: &[u8]) -> Signature {
+        match *self {
+            SecretKey::Ed25519(ref sk) => Signature::from(sign::sign_detached(message, sk)),
+        }
+    }
+
+    /// Return whether this `SecretKey` uses the ed25519 cryptographic primitive.
+    pub fn is_ed25519(&self) -> bool {
+        match *self {
+            SecretKey::Ed25519(_) => true,
+        }
+    }
 }
 
 impl From<sign::SecretKey> for SecretKey {
     fn from(ed25519_sk: sign::SecretKey) -> SecretKey {
         SecretKey::Ed25519(ed25519_sk)
+    }
+}
+
+/// Allows to access the byte contents of a `SecretKey` as a slice.
+impl Index<Range<usize>> for SecretKey {
+    type Output = [u8];
+    fn index(&self, _index: Range<usize>) -> &[u8] {
+        match *self {
+            SecretKey::Ed25519(ref sk) => sk.index(_index),
+        }
+    }
+}
+
+/// Allows to access the byte contents of a `SecretKey` as a slice.
+impl Index<RangeTo<usize>> for SecretKey {
+    type Output = [u8];
+    fn index(&self, _index: RangeTo<usize>) -> &[u8] {
+        match *self {
+            SecretKey::Ed25519(ref sk) => sk.index(_index),
+        }
+    }
+}
+
+/// Allows to access the byte contents of a `SecretKey` as a slice.
+impl Index<RangeFrom<usize>> for SecretKey {
+    type Output = [u8];
+    fn index(&self, _index: RangeFrom<usize>) -> &[u8] {
+        match *self {
+            SecretKey::Ed25519(ref sk) => sk.index(_index),
+        }
+    }
+}
+
+/// Allows to access the byte contents of a `SecretKey` as a slice.
+impl Index<RangeFull> for SecretKey {
+    type Output = [u8];
+    fn index(&self, _index: RangeFull) -> &[u8] {
+        match *self {
+            SecretKey::Ed25519(ref sk) => sk.index(_index),
+        }
+    }
+}
+
+/// An ssb signature. This type abstracts over the fact that ssb can support
+/// multiple cryptographic primitives.
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+pub enum Signature {
+    /// An [Ed25519](http://ed25519.cr.yp.to/) signature, as used by
+    /// `sodiumoxide::crypto::sign`.
+    Ed25519(sign::Signature),
+}
+
+impl Signature {
+    /// Return the length (in bytes) of the signature.
+    pub fn len(&self) -> usize {
+        match *self {
+            Signature::Ed25519(_) => sign::SIGNATUREBYTES,
+        }
+    }
+
+    /// Return whether this `Signature` uses the ed25519 cryptographic primitive.
+    pub fn is_ed25519(&self) -> bool {
+        match *self {
+            Signature::Ed25519(_) => true,
+        }
+    }
+}
+
+impl From<sign::Signature> for Signature {
+    fn from(ed25519_sig: sign::Signature) -> Signature {
+        Signature::Ed25519(ed25519_sig)
+    }
+}
+
+impl AsRef<[u8]> for Signature {
+    fn as_ref(&self) -> &[u8] {
+        match *self {
+            Signature::Ed25519(ref sig) => sig.as_ref(),
+        }
+    }
+}
+
+/// Allows to access the byte contents of a `Signature` as a slice.
+impl Index<Range<usize>> for Signature {
+    type Output = [u8];
+    fn index(&self, _index: Range<usize>) -> &[u8] {
+        match *self {
+            Signature::Ed25519(ref sig) => sig.index(_index),
+        }
+    }
+}
+
+/// Allows to access the byte contents of a `Signature` as a slice.
+impl Index<RangeTo<usize>> for Signature {
+    type Output = [u8];
+    fn index(&self, _index: RangeTo<usize>) -> &[u8] {
+        match *self {
+            Signature::Ed25519(ref sig) => sig.index(_index),
+        }
+    }
+}
+
+/// Allows to access the byte contents of a `Signature` as a slice.
+impl Index<RangeFrom<usize>> for Signature {
+    type Output = [u8];
+    fn index(&self, _index: RangeFrom<usize>) -> &[u8] {
+        match *self {
+            Signature::Ed25519(ref sig) => sig.index(_index),
+        }
+    }
+}
+
+/// Allows to access the byte contents of a `Signature` as a slice.
+impl Index<RangeFull> for Signature {
+    type Output = [u8];
+    fn index(&self, _index: RangeFull) -> &[u8] {
+        match *self {
+            Signature::Ed25519(ref sig) => sig.index(_index),
+        }
     }
 }
 
@@ -100,49 +351,3 @@ pub fn keypair_from_seed_ed25519(seed: &sign::Seed) -> (PublicKey, SecretKey) {
     let (pk, sk) = sign::keypair_from_seed(seed);
     (PublicKey::from(pk), SecretKey::from(sk))
 }
-
-// TODO Should this be added? This would make the abstraction leaky and would
-// also make adding new primitives a breaking change.
-// /// The different cryptographic primitives that can be used for ssb keys.
-// ///
-// /// Currently, only one such primitive ([Ed25519](http://ed25519.cr.yp.to/)) is
-// /// supported.
-// // TODO derive traits?
-// pub enum CryptoPrimitive {
-//     /// The [Ed25519](http://ed25519.cr.yp.to/) cryptographic primitive.
-//     Ed25519,
-// }
-//
-// impl CryptoPrimitive {
-//     /// Randomly generate a secret key and a corresponding public key, using the
-//     /// given primitive.
-//     ///
-//     /// THREAD SAFETY: `gen_keypair()`` is thread-safe provided that you have called
-//     /// `sodiumoxide::init()`` once before using any other function from sodiumoxide.
-//     pub fn gen_keypair(&self) -> (PublicKey, SecretKey) {
-//         match *self {
-//             CryptoPrimitive::Ed25519 => gen_keypair_ed25519(),
-//         }
-//     }
-//
-//     /// Return the number of bytes in a public key of this primitive.
-//     pub fn public_key_bytes(&self) -> usize {
-//         match *self {
-//             CryptoPrimitive::Ed25519 => sign::PUBLICKEYBYTES,
-//         }
-//     }
-//
-//     /// Return the number of bytes in a secret key of this primitive.
-//     pub fn secret_key_bytes(&self) -> usize {
-//         match *self {
-//             CryptoPrimitive::Ed25519 => sign::SECRETKEYBYTES,
-//         }
-//     }
-//
-//     /// Return the number of bytes in a signature of this primitive.
-//     pub fn signature_bytes(&self) -> usize {
-//         match *self {
-//             CryptoPrimitive::Ed25519 => sign::SIGNATUREBYTES,
-//         }
-//     }
-// }
