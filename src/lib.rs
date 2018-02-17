cd07f88f9e4a4f9ea21a9331dbab6e5cbf162919
AljoschaMeyer <mail@aljoscha-meyer.de>
2018-02-17 21:14:36 +0100

    Add some useful constants

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+//! Common types and data for the ssb ecosystem
+#![deny(missing_docs)]
+
+extern crate sodiumoxide;
+
+/// The network identifier of the main ssb network.
+const MAINNET_IDENTIFIER: [u8; 32] = [212, 161, 203, 136, 166, 111, 2, 248, 219, 99, 92, 226, 100,
+                                      65, 204, 93, 172, 27, 8, 66, 12, 234, 172, 35, 8, 57, 183,
+                                      85, 132, 90, 159, 251];
+
+/// The name of the directory where ssb stores its data by default.
+///
+/// Note that ssb always looks for a hidden directory, so the actual path used
+/// will be `".ssb"`, not `"ssb"`. The directory is resolved from the home
+/// directory of the user. So a full path could for example be
+/// `"/home/foobar/.ssb"` (on linux).
+const DEFAULT_SSB_DIRECTORY_NAME: &'static str = "ssb";
+
+/// Applications should read this environment variable to use an ssb directory
+/// other than the default one.
+const ENV_SSB_DIRECTORY_NAME: &'static str = "ssb_appname";
+
+pub mod keys;
